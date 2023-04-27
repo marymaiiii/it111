ea6df3995dd88240e9e94bc460d98fa8f0bec1f7
Mai Nguyen <122258917+marymaiiii@users.noreply.github.com>
2023-04-27 10:25:59 -0700

    Add files via upload

---
+++ week4/src/MyPlaylist.java
@@ -0,0 +1,12 @@
+public class MyPlaylist {
+    public static void main(String[] args) {
+        int playlist = 1;
+
+        while(playlist <= 10) {
+            System.out.println("playing my songs!");
+            playlist++;
+        }
+            System.out.println("Finished playing my songs!");
+
+    }
+}
